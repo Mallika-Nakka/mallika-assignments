598a0db7accf50f4907f91d7a50160bbfa98fd21
Mallika Nakka <mallika.nakka@acheron-tech.com>
2021-10-19 14:55:05 +0530

    third commit

--- Assignments/src/com/shristi/basic/EvenNumbers.java
+++ Assignments/src/com/shristi/basic/EvenNumbers.java
@@ -6,23 +6,20 @@ public class EvenNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner sc=new Scanner(System.in);
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first NUmber");
-		int firstNumber=sc.nextInt();
+		int firstNumber = sc.nextInt();
 		System.out.println("Enter second NUmber");
-		int secondNumber=sc.nextInt();
-		
-		
-		
-		
-		for (int i=firstNumber;i<=secondNumber;i++) {
-			
-			if (i%2==0) {
+		int secondNumber = sc.nextInt();
+
+		for (int i = firstNumber; i <= secondNumber; i++) {
+
+			if (i % 2 == 0) {
 				System.out.println(i);
-				
+
 			}
-			
+
 		}
 
 	}
